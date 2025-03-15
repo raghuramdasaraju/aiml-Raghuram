d4431de90d43f6aa2d8cc286eea28585c1c12c6b
raghuramdasaraju <raghudasaraju0122@gmail.com>
2025-03-15 10:19:43 +0530

    Add files via upload

---
+++ pima_app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "232060de-7746-4d32-a4b0-52474d40d989",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
